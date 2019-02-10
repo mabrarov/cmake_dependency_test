ea62f1f119d1559cb9a66fdada8f81d9ee9ace48
Marat Abrarov <abrarov@gmail.com>
2019-02-10 23:06:22 +0300

    Fixed reference to license in copyright headers

--- static_lib_a/include/static_lib_a.h
+++ static_lib_a/include/static_lib_a.h
@@ -1,8 +1,7 @@
 //
 // Copyright (c) 2016 Marat Abrarov (abrarov@gmail.com)
 //
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+// Distributed under the MIT License. (See accompanying LICENSE)
 //
 
 #ifndef DEPENDENCY_TEST_STATIC_LIB_A_H
